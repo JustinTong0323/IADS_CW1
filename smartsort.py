8068caf25747708b36eeade1a519d45ffc81ec07
XinyuanTong <115166877+JustinTong0323@users.noreply.github.com>
2023-11-08 15:51:34 +0000

    complete T2

--- smartsort.py
+++ smartsort.py
@@ -20,10 +20,10 @@ insertSortThreshold = 10
 
 sortedRunThreshold = 10
 
-
 # TODO: Task 1. Hybrid Merge/Insert Sort
 
 # In-place Insert Sort on A[m],...,A[n-1]:
+
 
 def insertSort(A, m, n):
     # iterate over the sublist A[m+1:n]
@@ -64,6 +64,7 @@ def merge(C, D, m, p, n):
         j += 1
         k += 1
 
+
 # Merge Sort A[m],...,A[n-1] using just B[m],...,B[n-1] as workspace,
 # deferring to Insert Sort if length <= insertSortThreshold
 
@@ -98,40 +99,96 @@ def greenMergeSortAll(A):
     return A
 
 
-if __name__ == "__main__":
-    # generate a list of random numbers
-    import random
-    random.seed(123)
-    A = [random.randint(0, 100) for _ in range(100)]
-    print(A)
-    # sort it
-    A = greenMergeSortAll(A)
-    print(A)
-
 # TODO: Task 2. Detecting already sorted runs.
 
 # Build and return queue of sorted runs of length >= sortedRunThreshold
 # Queue items are pairs (i,j) where A[i],...,A[j-1] is sorted
 
-#   allSortedRuns(A)
+
+def allSortedRuns(A):
+    Q = PeekQueue()  # Initialize a PeekQueue to store sorted runs
+    i = 0  # Initialize a pointer to the start of the list
+    while i < len(A):
+        j = i + 1
+        # Find the end of the current sorted run
+        while j < len(A) and comp(A[j - 1], A[j]):
+            j += 1
+        # If the sorted run is long enough, add it to the PeekQueue
+        if j - i >= sortedRunThreshold:
+            Q.push((i, j))
+        i = j  # Move the pointer to the start of the next run
+    return Q
+
 
 # Test whether A[i],...,A[j-1] is sorted according to information in Q
 
-#   isWithinRun(Q,i,j)
+
+def isWithinRun(Q, i, j):
+    # iterate over the queue
+    while Q.peek():
+        # get the next pair of indices
+        (a, b) = Q.peek()
+        # check if the pair of indices matches the given pair
+        if a > i:
+            return False
+        if j <= b:
+            return True
+        if i < b:
+            break
+        Q.pop()  # remove the pair of indices from the queue
+    return False
+
 
 # Improvement on greenMergeSort taking advantage of sorted runs
 
-#   smartMergeSort(A,B,Q,m,n)
+def smartMergeSort(A, B, Q, m, n):
+    # If the list is already sorted, do nothing
+    flag = isWithinRun(Q, m, n)
+    if flag:
+        # print("m:", m, "n:", n)
+        return
+
+    # If the length of the list is less than or equal to insertSortThreshold, use insertion sort\
+    if n - m <= insertSortThreshold:
+        insertSort(A, m, n)
+    else:
+
+        # Divide the list into four sublists
+        q = (m + n) // 2
+        p = (m + q) // 2
+        r = (q + n) // 2
+
+        # Recursively sort the sublists
+        smartMergeSort(A, B, Q, m, p)
+        smartMergeSort(A, B, Q, p, q)
+        smartMergeSort(A, B, Q, q, r)
+        smartMergeSort(A, B, Q, r, n)
+
+        # Merge the sublists
+        merge(A, B, m, p, q)
+        merge(A, B, q, r, n)
+        merge(B, A, m, q, n)
 
 
 # Provided code:
 
-# def smartMergeSortAll(A):
-#     B = [None] * len(A)
-#     Q = allSortedRuns(A)
-#     smartMergeSort(A, B, Q, 0, len(A))
-#     return A
+def smartMergeSortAll(A):
+    B = [None] * len(A)
+    Q = allSortedRuns(A)
+    smartMergeSort(A, B, Q, 0, len(A))
+    return A
 
+
+# for testing:
+if __name__ == "__main__":
+    # generate a list of random integers
+    import random
+    random.seed(323)
+    A = [random.randint(0, 1000) for _ in range(1000)]
+    greenMergeSort(A, [None] * 1000, 200, 900)
+    # print(A)
+    smartMergeSortAll(A)
+    print(A)
 
 # TODO: Task 3. Asymptotic analysis of smartMergeSortAll
 
